40c836b779a292814d183ed44bcf0c6955f4203c
Peter Heslin <pheslin@gmail.com>
2019-01-14 22:17:29 +0000

    reformat_lsj.pl can now process Helma's Logeion version.

--- utils/reformat_lsj.pl
+++ utils/reformat_lsj.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl -w
-# Unfortunately Perseus updated the formatting of their XML files
-# to be more usable, which broke our naive XML parsing. This script
-# returns the XML to its less readable form, as that is easier than
-# converting all of Diogenes to do proper XML parsing.
-
 use strict;
+
+# Unfortunately Perseus updated the formatting of their XML files to be more usable, which broke our naive XML parsing. This script returns the XML to its less readable form, as that is easier than converting all of Diogenes to do proper XML parsing.
+
+# The -l option is to process the forked version of the Perseus LSJ of the Logeion project.  Helma Dik has corrected many errors in the original Perseus XML translation of the LSJ.  Some of the tags have been changed and the beta code has been converted into Unicode.  Many of the corrections involve splitting entries that were wrongly compressed into one when the XML was created, but Morpheus would need to be rebuilt to take advantage of that.
+
+my $logeion;
+$logeion = 1 if @ARGV and $ARGV[0] eq '-l';
 
 use XML::Parser;
 use XML::Parser::EasyTree;
@@ -37,6 +39,9 @@ sub print_contents {
                 printf("%s", $content);
             }
         } else {
+            if ($logeion and $item->{'name'} eq 'div2') {
+                $item->{'name'} = 'entryFree';
+            }
             if($item->{'name'} eq 'entryFree') {
                 $inentry = 1;
             }
