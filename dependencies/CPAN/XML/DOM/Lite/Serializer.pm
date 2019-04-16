b76ce0f46f90efc9cdd42d03d09ccca51f8619c4
dkl0pjh <pheslin@gmail.com>
2019-04-16 12:31:13 +0100

    Abortive effort to fix indentation in Serializer.
    To indent XML w/o introducing significant whitespace is a harder
    problem than can be addressed here.  And libxml does not indent when
    serializing, so doing it here makes it harder to compare output.
    Easier just to use xmllint vel sim.

--- dependencies/CPAN/XML/DOM/Lite/Serializer.pm
+++ dependencies/CPAN/XML/DOM/Lite/Serializer.pm
@@ -5,53 +5,82 @@ use strict;
 use XML::DOM::Lite::Constants qw(:all);
 
 sub new {
-    my $class = shift;
-    return bless { }, $class;
+    my ($class, %options) = @_;
+    my $self = bless { }, $class;
+
+    $self->{_mode} = '';
+    $self->{_newline} = "\n";
+    $self->{_space} = " ";
+    if (defined($options{'whitespace'})) {
+        my $mode = $options{'whitespace'};
+        if (index($mode, 'safe') >= 0) {
+            $self->{_mode} = 'safe';
+            $self->{_newline} = '';
+            $self->{_space} = ' ';
+        }
+        elsif (index($mode, 'none') >= 0) {
+            $self->{_none} = 'none';
+            $self->{_newline} = '';
+            $self->{_space} = '';
+        }
+    }
+    return $self;
 }
 
 sub serializeToString {
-    my ($self, $node) = @_;
+    my ($self, $node, %options) = @_;
     unless (ref $self) {
         $self = __PACKAGE__->new;
     }
-    my $out = "";
+
+    $self->{_indent_level} = 0 unless defined $self->{_indent_level};
+    $self->{_out} = "" unless defined $self->{_out};
 
     if ($node->nodeType == DOCUMENT_NODE) {
         foreach my $n (@{$node->childNodes}) {
-            $out .= $self->serializeToString($n);
+            $self->serializeToString($n);
         }
     }
 
-    $self->{_indent_level} = 0 unless defined $self->{_indent_level};
-
     if ($node->nodeType == ELEMENT_NODE) {
-        $out .= "".$self->_mkIndent()."<".$node->tagName;
+        $self->{_out} .= $self->{_newline}.$self->_mkIndent()."<".$node->tagName;
         foreach my $att (@{$node->attributes}) {
-            $out .= " $att->{nodeName}=\"".$att->{nodeValue}."\"";
+            $self->{_out} .= " $att->{nodeName}=\"".$att->{nodeValue}."\"";
         }
         if ($node->childNodes->length) {
-            $out .= ">";
+            $self->{_out} .= ">";
             $self->{_indent_level}++;
             foreach my $n (@{$node->childNodes}) {
-                $out .= $self->serializeToString($n);
+                $self->serializeToString($n);
             }
             $self->{_indent_level}--;
-            $out .= "".$self->_mkIndent()."</".$node->tagName.">";
+            $self->{_out} .= $self->{_newline}.$self->_mkIndent()."</".$node->tagName.">";
         } else {
-            $out .= " />";
+            $self->{_out} .= " />";
         }
     }
     elsif ($node->nodeType == TEXT_NODE) {
-        $out .= "".$self->_mkIndent().$node->nodeValue;
+        $self->{_out} .= $self->{_newline}.$self->_mkIndent().$node->nodeValue;
     }
     elsif ($node->nodeType == PROCESSING_INSTRUCTION_NODE) {
-        $out .= "<?".$node->nodeValue."?>";
+        $self->{_out} .= "<?".$node->nodeValue."?>";
     }
-    return $out;
+    return $self->{_out};
 }
 
 sub _mkIndent {
     my ($self) = @_;
-    return ("" x (2 * $self->{_indent_level}));
+    return '' if $self->{_none};
+    $self->{_out} =~ m/(\s*\n\s*)$/s;
+    print ":".$1.":\n" if $1;
+    if ($self->{_mode} eq 'safe') {
+        if ($self->{_out} =~ s/\s*\n\s*$/\n/s) {
+            return ($self->{_space} x (2 * $self->{_indent_level}));
+        }
+        else {
+            return '';
+        }
+    }
+    return ($self->{_space} x (2 * $self->{_indent_level}));
 }
 1;
